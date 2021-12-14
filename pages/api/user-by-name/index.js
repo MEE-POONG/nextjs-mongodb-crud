648c32ecfda013600d544925700970de7f71798e
ChunWarayut <warayut2555@gmail.com>
2021-12-14 19:14:37 +0700

    ADD OPTION

--- pages/api/user-by-name/index.js
+++ pages/api/user-by-name/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     switch (method) {
         case 'POST':
             try {
-                const users = await User.find({ firstname: { $regex: req.body.firstname } })
+                const users = await User.find({ firstname: { $regex: req.body.firstname, $options: 'imxs' } })
                 res.status(200).json({ success: true, data: users })
             } catch (error) {
                 res.status(400).json({ success: false })
